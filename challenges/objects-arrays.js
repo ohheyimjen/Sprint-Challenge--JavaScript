123e1617d216e5507a8a4785b377bfd3cda82a61
ohheyimjen <jendority@gmail.com>
2019-03-08 13:57:44 -0800

    changed 'dinosaur' object w 3 arrays into 3 individual objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -39,22 +39,28 @@ const dinosaur = [
   },
 
 ]
+const tyrannosaurus = [diet: 'carnivorous' weight: '7000kg' length: '12m' period: 'Late Cretaceious',
+roar: () => 'RAWERSRARARWERSARARARRRR!',];
+
+const stegosaurus = [diet: 'herbivorous' weight: '2000kg' length: '9m' period: 'Late Jurassic'];
+
+const velociraptor = [diet: 'carnivorous' weight: '15kg' length: '1.8m' period: 'Late Cretaceious'];
 
 // How much did tyrannosaurus weigh?
-console.log(dinosaur[0].weight);
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log(dinosaur[2].diet);
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log(dinosaur[1].length);
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log(dinosaur[0].period);
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log(dinosaur[0].roar());
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
